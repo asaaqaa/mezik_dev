28b55252906e82ff6274f82aa859288c3dfe1fc8
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-08 01:59:14 +0300

    Update song.py

--- AnonX/utils/inline/song.py
+++ AnonX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="الدعم", url=f"{config.SUPPORT_GROUP}",
+                text="ليطمئن قلبي", url=f"{config.SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
